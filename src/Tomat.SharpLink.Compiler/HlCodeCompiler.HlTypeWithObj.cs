35f8c2c2474bddee57c579b0bd9f465366b0c7ef
Tomat <xxlennygamerxx@gmail.com>
2023-08-02 18:12:33 -0700

    Begin defining func fields.

--- src/Tomat.SharpLink.Compiler/HlCodeCompiler.HlTypeWithObj.cs
+++ src/Tomat.SharpLink.Compiler/HlCodeCompiler.HlTypeWithObj.cs
@@ -6,6 +6,7 @@ namespace Tomat.SharpLink.Compiler;
 partial class HlCodeCompiler {
     private Dictionary<HlTypeWithObj, TypeDefinition> objDefs = new();
     private Dictionary<HlTypeWithObj, Dictionary<HlObjField, FieldDefinition>> objFieldDefs = new();
+    private Dictionary<HlTypeWithObj, Dictionary<HlObjProto, FieldDefinition>> objProtoDefs = new();
 
     private void ResolveHlTypeWithObj(HlTypeWithObj type, AssemblyDefinition asmDef) {
         ExtractNameAndNamespace(type.Obj.Name, out var ns, out var name);
@@ -29,6 +30,18 @@ partial class HlCodeCompiler {
             );
             fieldDefs.Add(field, fieldDef);
         }
+
+        var protoDefs = objProtoDefs[type] = new Dictionary<HlObjProto, FieldDefinition>();
+
+        foreach (var proto in type.Obj.Protos) {
+            // TODO: Handle property stuff...
+            var protoDef = new FieldDefinition(
+                proto.Name,
+                FieldAttributes.Public,
+                asmDef.MainModule.TypeSystem.Object // temporary
+            );
+            protoDefs.Add(proto, protoDef);
+        }
     }
 
     private void DefineHlTypeWithObj(HlTypeWithObj type, AssemblyDefinition asmDef) {
@@ -38,6 +51,11 @@ partial class HlCodeCompiler {
         foreach (var field in type.Obj.Fields) {
             var fieldDef = objFieldDefs[type][field];
             fieldDef.FieldType = TypeReferenceFromHlTypeRef(field.Type, asmDef);
+        }
+
+        foreach (var proto in type.Obj.Protos) {
+            var protoDef = objProtoDefs[type][proto];
+            protoDef.FieldType = funDefs[(HlTypeWithFun)code.Functions[proto.FIndex].Type.Value!];
         }
     }
 
