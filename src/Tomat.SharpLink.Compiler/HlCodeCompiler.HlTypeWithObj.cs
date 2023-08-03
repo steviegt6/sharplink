d26a7cd50053192f033fae814c2a03e885db4d64
Tomat <xxlennygamerxx@gmail.com>
2023-08-02 17:14:24 -0700

    Generate fields from `HlTypeWithObj`.

--- src/Tomat.SharpLink.Compiler/HlCodeCompiler.HlTypeWithObj.cs
+++ src/Tomat.SharpLink.Compiler/HlCodeCompiler.HlTypeWithObj.cs
@@ -5,6 +5,7 @@ namespace Tomat.SharpLink.Compiler;
 
 partial class HlCodeCompiler {
     private Dictionary<HlTypeWithObj, TypeDefinition> objDefs = new();
+    private Dictionary<HlTypeWithObj, Dictionary<HlObjField, FieldDefinition>> objFieldDefs = new();
 
     private void ResolveHlTypeWithObj(HlTypeWithObj type, AssemblyDefinition asmDef) {
         ExtractNameAndNamespace(type.Obj.Name, out var ns, out var name);
@@ -17,15 +18,35 @@ partial class HlCodeCompiler {
             asmDef.MainModule.TypeSystem.Object // temporary
         );
         objDefs.Add(type, objDef);
+
+        var fieldDefs = objFieldDefs[type] = new Dictionary<HlObjField, FieldDefinition>();
+
+        foreach (var field in type.Obj.Fields) {
+            var fieldDef = new FieldDefinition(
+                field.Name,
+                FieldAttributes.Public,
+                asmDef.MainModule.TypeSystem.Object // temporary
+            );
+            fieldDefs.Add(field, fieldDef);
+        }
     }
 
     private void DefineHlTypeWithObj(HlTypeWithObj type, AssemblyDefinition asmDef) {
         var objDef = objDefs[type];
         objDef.BaseType = type.Obj.Super is null ? asmDef.MainModule.TypeSystem.Object : TypeReferenceFromHlTypeRef(type.Obj.Super, asmDef);
+
+        foreach (var field in type.Obj.Fields) {
+            var fieldDef = objFieldDefs[type][field];
+            fieldDef.FieldType = TypeReferenceFromHlTypeRef(field.Type, asmDef);
+        }
     }
 
     private void CompileHlTypeWithObj(HlTypeWithObj type, AssemblyDefinition asmDef) {
         var objDef = objDefs[type];
         asmDef.MainModule.Types.Add(objDef);
+
+        var fieldDefs = objFieldDefs[type];
+        foreach (var fieldDef in fieldDefs.Values)
+            objDef.Fields.Add(fieldDef);
     }
 }
